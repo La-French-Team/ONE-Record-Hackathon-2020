47b2b3d956a80ef442be3da8a3a8a2c6a0f6c2ef
acouvreur <acouvreur@users.noreply.github.com>
2020-09-11 20:07:38 +0000

    deploy: 20e83c6dd4f30dc17d0e5d6221133343e134abb6

---
+++ precache-manifest.5d62875768dab78828a52f702aa0d6c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5317c5392b5b4139c5fb86a79f1940a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c07221dc02c918d68c0c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "58f9db8a900e76f3de5b",
+    "url": "/static/js/2.a9d4e2bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a9d4e2bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c07221dc02c918d68c0c",
+    "url": "/static/js/main.7dcfa8b7.chunk.js"
+  },
+  {
+    "revision": "f9725154d351a8a9f56f",
+    "url": "/static/js/runtime-main.19d5dfce.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
