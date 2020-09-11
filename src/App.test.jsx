1176f07f4fdb4ea7b6ed714d8dea64f25a874bc2
Alexis Couvreur <alexiscouvreur.pro@gmail.com>
2020-09-12 00:42:59 +0200

    Fix test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/About/i);
+  const linkElement = getByText(/ONE Record Hackathon 2020/i);
   expect(linkElement).toBeInTheDocument();
 });
