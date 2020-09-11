bfbdc210ed92267713a0538c4b2c4d8a6368fd11
Alexis Couvreur <alexiscouvreur.pro@gmail.com>
2020-09-12 00:41:00 +0200

    Changed copyright link

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ function Copyright() {
   return (
     <Typography variant='body2' color='textSecondary' align='center'>
       {'Copyright © '}
-      <Link color='inherit' href='https://material-ui.com/'>
+      <Link color='inherit' href='http://onerecord.fr/'>
         The French Team
       </Link>{' '}
       {new Date().getFullYear()}
