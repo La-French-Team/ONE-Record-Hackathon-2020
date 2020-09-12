581978c708ca65602babc381caef3a5f14fa3cab
alexandre.hiltcher@etu.unice.fr <ahiltcher@supralog.com>
2020-09-12 15:00:17 +0200

    Remove vertical flex in centrail details container

--- src/components/commons/Page/Page.jsx
+++ src/components/commons/Page/Page.jsx
@@ -1,21 +1,11 @@
 import React from 'react';
 import Header from 'components/commons/Header/Header';
-import { makeStyles } from '@material-ui/core';
-
-const useStyle = makeStyles(() => ({
-  body: {
-    overflowY: 'auto',
-    flex: '1 1 auto',
-    height: '0',
-  },
-}));
 
 const Page = ({ pageName, children }) => {
-  const classes = useStyle();
   return (
     <>
       <Header pageName={pageName} />
-      <div className={classes.body}>{children}</div>
+      {children}
     </>
   );
 };
--- src/pages/Shipment.jsx
+++ src/pages/Shipment.jsx
@@ -1,11 +1,5 @@
 import React from 'react';
-import {
-  Grid,
-  Typography,
-  useMediaQuery,
-  Box,
-  makeStyles,
-} from '@material-ui/core';
+import { Grid, useMediaQuery, makeStyles } from '@material-ui/core';
 import ShipmentStatus from 'components/shipment/ShipmentStatus';
 import LineChart from 'components/stats/LineChart/LineChart';
 import Page from 'components/commons/Page/Page';
@@ -14,17 +8,16 @@ import Piece from 'components/piece/Piece';
 import Event from 'components/event/Event';
 
 const useStyle = makeStyles(() => ({
-  fullHeightContainer: {
-    height: '100%',
-  },
   mapContainer: {
     width: '100%',
-    flex: '2 2 auto',
-    maxHeight: '50%',
+    height: '600px',
   },
   detailsContainer: {
-    flex: '1 1 auto',
-    height: '0',
+    height: '300px',
+  },
+  chartContainer: {
+    margin: '10px 0',
+    height: '400px',
   },
 }));
 
@@ -32,18 +25,11 @@ export default () => {
   const classes = useStyle();
   return (
     <Page pageName='Shipment details'>
-      <Grid container spacing={0} className={classes.fullHeightContainer}>
+      <Grid container spacing={0}>
         <Grid item xs={12} md={2}>
           <PieceList />
         </Grid>
-        <Grid
-          container
-          item
-          xs={12}
-          md={8}
-          direction='column'
-          className={classes.fullHeightContainer}
-        >
+        <Grid container item xs={12} md={8} direction='column'>
           <Grid item>
             <ShipmentStatus />
           </Grid>
@@ -93,8 +79,8 @@ const EventList = () => {
 const ShipmentDetails = () => {
   const classes = useStyle();
   return (
-    <Grid container className={classes.fullHeightContainer}>
-      <Grid item xs={12} md={6} style={{ height: '100%' }}>
+    <Grid container>
+      <Grid item xs={12} className={classes.chartContainer}>
         <LineChart
           series={[
             {
@@ -112,8 +98,23 @@ const ShipmentDetails = () => {
           ]}
         />
       </Grid>
-      <Grid item xs={12} md={6}>
-        test
+      <Grid item xs={12} className={classes.chartContainer}>
+        <LineChart
+          series={[
+            {
+              id: 'Hygrometry',
+              data: [
+                { x: 0, y: 7 },
+                { x: 1, y: 5 },
+                { x: 2, y: 11 },
+                { x: 3, y: 9 },
+                { x: 4, y: 13 },
+                { x: 7, y: 16 },
+                { x: 9, y: 12 },
+              ],
+            },
+          ]}
+        />
       </Grid>
     </Grid>
   );
