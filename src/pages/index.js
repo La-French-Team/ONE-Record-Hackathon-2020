80a4f581c563f5382341516aa7109da7420b8d01
Alexis Couvreur <alexiscouvreur.pro@gmail.com>
2020-09-12 10:27:04 +0200

    Added pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,7 @@
 import Home from './Home';
 import About from './About';
+import Login from './Login';
+import Overview from './Overview';
+import Details from './Details';
 
-export { Home, About };
+export { Home, About, Login, Overview };
