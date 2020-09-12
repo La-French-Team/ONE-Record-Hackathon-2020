77ca253550d0bcc12291a7067813c9ca3c14ac22
alexandre.hiltcher@etu.unice.fr <ahiltcher@supralog.com>
2020-09-12 11:34:23 +0200

    Add style to login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -7,32 +7,46 @@ const useStyles = makeStyles(() => ({
   linkButton: {
     textDecoration: 'none',
   },
+  item: {
+    display: 'flex',
+    alignItem: 'center',
+    justifyContent: 'space-around',
+  },
 }));
 
 export default () => {
   const classes = useStyles();
   return (
     <Page pageName='Login'>
-      <Grid container spacing={12}>
-        <Grid item xs={4}>
+      <Grid container spacing={10} justify='space-between'>
+        <Grid item className={classes.item} xs={12}>
+          <h2>Sign In</h2>
+        </Grid>
+        <Grid item className={classes.item} xs={4}>
           <Link to='shipment/1' className={classes.linkButton}>
-            <Button>
-              <h2>Login as shipper</h2>
+            <Button variant='contained' color='primary'>
+              <h2>I am a shipper</h2>
             </Button>
           </Link>
         </Grid>
-        <Grid item xs={4}>
-          <Button>
-            <h2>Login as airline</h2>
+        <Grid item className={classes.item} xs={4}>
+          <Button variant='contained' color='primary'>
+            <h2>I am an airline</h2>
           </Button>
         </Grid>
-        <Grid item xs={4}>
-          <Button>
-            <h2>Login as guest</h2>
+        <Grid item className={classes.item} xs={4}>
+          <Button variant='contained' color='primary'>
+            <h2>I am a guest</h2>
           </Button>
         </Grid>
-        <Grid item xs={12}>
-          <Button>
+        <Grid
+          item
+          justify='space-around'
+          alignItems='center'
+          className={classes.item}
+          xs={12}
+        >
+          <Button variant='contained' color='secondary'>
             <h2>Sign up</h2>
           </Button>
         </Grid>
