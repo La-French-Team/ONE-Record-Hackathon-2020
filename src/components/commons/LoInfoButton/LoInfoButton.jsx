239277b9001562b087eec22c6f49dc9a6305a425
Thomas Moreau <tmoreau@supralog.com>
2020-09-13 02:16:54 +0200

    add QrCode Modal to Pieces

--- src/components/commons/LoInfoButton/LoInfoButton.jsx
+++ src/components/commons/LoInfoButton/LoInfoButton.jsx
@@ -1,12 +1,109 @@
-import React from 'react';
-import QrCodeIcon from 'components/icons/QrCode';
+import React, { useCallback } from "react";
+import { makeStyles } from "@material-ui/core";
+import QrCodeIcon from "components/icons/QrCode";
+import { withStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import MuiDialogTitle from "@material-ui/core/DialogTitle";
+import MuiDialogContent from "@material-ui/core/DialogContent";
+import MuiDialogActions from "@material-ui/core/DialogActions";
+import IconButton from "@material-ui/core/IconButton";
+import CloseIcon from "@material-ui/icons/Close";
+import Typography from "@material-ui/core/Typography";
 
-const LoInfoButton = ({ loUri }) => {
+const useStyles = makeStyles({
+  qrcode: {
+    cursor: "pointer",
+    float: "right",
+    color: "white",
+    "background-color": "#2196f3",
+    "border-radius": "50%",
+    padding: "5px",
+  },
+});
+
+const styles = (theme) => ({
+  root: {
+    margin: 0,
+    padding: theme.spacing(2),
+  },
+  closeButton: {
+    position: "absolute",
+    right: theme.spacing(1),
+    top: theme.spacing(1),
+    color: theme.palette.grey[500],
+  },
+});
+
+const DialogTitle = withStyles(styles)((props) => {
+  const { children, classes, onClose, ...other } = props;
+  return (
+    <MuiDialogTitle disableTypography className={classes.root} {...other}>
+      <Typography variant="h6">{children}</Typography>
+      {onClose ? (
+        <IconButton
+          aria-label="close"
+          className={classes.closeButton}
+          onClick={onClose}
+        >
+          <CloseIcon />
+        </IconButton>
+      ) : null}
+    </MuiDialogTitle>
+  );
+});
+
+const DialogContent = withStyles((theme) => ({
+  root: {
+    padding: theme.spacing(2),
+  },
+}))(MuiDialogContent);
+
+const DialogActions = withStyles((theme) => ({
+  root: {
+    margin: 0,
+    padding: theme.spacing(1),
+  },
+}))(MuiDialogActions);
+
+const LoInfoButton = ({ loUri, loType }) => {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  const onButtonClick = () => () => {
+    setOpen(true);
+  };
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <>
-      <button>
-        <QrCodeIcon></QrCodeIcon>
-      </button>
+      <QrCodeIcon
+        className={classes.qrcode}
+        onClick={onButtonClick()}
+      ></QrCodeIcon>
+      <Dialog
+        onClose={handleClose}
+        aria-labelledby="customized-dialog-title"
+        open={open}
+      >
+        <DialogTitle id="customized-dialog-title" onClose={handleClose}>
+          <div style={{ minWidth: "300px" }}>{loType} - Link (ONE Record)</div>
+        </DialogTitle>
+        <DialogContent dividers>
+          <a href={loUri}>{loUri}</a>
+        </DialogContent>
+        <DialogActions>
+          <Button autoFocus onClick={handleClose} color="primary">
+            Close
+          </Button>
+        </DialogActions>
+      </Dialog>
     </>
   );
 };
